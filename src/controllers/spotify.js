880f4e695111087a1371b8be6b3b922de4f624ef
David Barratt <david@davidwbarratt.com>
2018-06-09 14:50:48 -0400

    Add Spotify Search

--- src/controllers/spotify.js
+++ src/controllers/spotify.js
@@ -8,7 +8,17 @@ const search = (request) => {
 
   return createSpotify().then(spotify => (
     spotify.searchTracks(request.query.query)
-  )).then(data => data.body.tracks.items);
+  )).then(data => (
+    data.body.tracks.items.map(item => (
+      {
+        id: item.id,
+        name: item.name,
+        artists: item.artists.map(artist => artist.name),
+        album: item.album.name,
+        image: item.album.images[item.album.images.length - 1].url,
+      }
+    ))
+  ));
 };
 
 export default search;
